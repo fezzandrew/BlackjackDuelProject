80c02369124db0f82a098f0bc447199ae66ee0f4
fezzandrew <46875660+fezzandrew@users.noreply.github.com>
2019-03-05 23:56:36 -0600

    Add files via upload

--- app/src/main/java/com/example/fezzdrew/blackjackduel/MainActivity.java
+++ app/src/main/java/com/example/fezzdrew/blackjackduel/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fezzdrew.blackjackduel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //menu buttons
+        Button buttonLowTable = findViewById(R.id.btnLowStakes);
+        buttonLowTable.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.low_stakes_game_view);
+            }
+        });
+
+        Button buttonHighTable = findViewById(R.id.btnHighStakes);
+        buttonHighTable.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.high_stakes_game_view);
+            }
+        });
+
+        Button buttonRules = findViewById(R.id.btnRules);
+        buttonRules.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.rules);
+            }
+        });
+
+        Button buttonAccount = findViewById(R.id.btnAccount);
+        buttonAccount.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.account);
+            }
+        });
+/*
+        Button buttonReturn = findViewById(R.id.btnReturn);
+        buttonReturn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+
+        });
+        */
+        //menu end
     }
 }
